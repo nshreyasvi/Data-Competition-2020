ae6867aba7b9ace782c4ec83827e260893cb4c00
nshreyasvi <shreyasvi.bt15@rvce.edu.in>
2020-05-05 08:17:48 +0200

    changes

---
+++ cnn.R
@@ -0,0 +1,47 @@
+rm(list=ls())
+library(caret)
+library(caTools)
+library(h2o)
+library(keras)
+
+#set.seed(123)
+dataset <- read.csv('train.csv')
+test <- read.csv('test.csv')
+
+dataset$y = factor(dataset$y, levels = c(0, 1))
+#dataset$days_elapsed_old[dataset$days_elapsed_old<1] <- 0
+
+split = sample.split(dataset$y, SplitRatio = 0.75)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+local.h2o <- h2o.init(ip = "localhost", port = 54321, startH2O = TRUE, nthreads=-1)
+trData<-as.h2o(training_set)
+tsData<-as.h2o(test_set)
+
+res.dl <- h2o.deeplearning(x = 1:16, y = 17, trData, activation = "Tanh", hidden=rep(160,5),epochs = 20)
+
+#use model to predict testing dataset
+pred.dl<-h2o.predict(object=res.dl, newdata=tsData[,-17])
+pred.dl.df<-as.data.frame(pred.dl)
+
+summary(pred.dl)
+test_labels<-test[,17]
+
+#calculate number of correct prediction
+#sum(diag(table(test_labels,pred.dl.df[,17])))
+confusionMatrix(table(test_labels,pred.dl.df[,17]))
+
+#================================================================================
+# read test.csv
+#test<-read.csv("test.csv")
+
+#test_h2o<-as.h2o(test)
+
+# convert H2O format into data frame and save as csv
+#df.test <- as.data.frame(pred.dl.test)
+#df.test <- data.frame(ImageId = seq(1,length(df.test$predict)), Label = df.test$predict)
+#write.csv(df.test, file = "submission.csv", row.names=FALSE)
+
+# shut down virtual H2O cluster
+h2o.shutdown(prompt = FALSE)
---
+++ cnn_keras.R
@@ -0,0 +1,57 @@
+rm(list=ls())
+library(keras)
+library(caret)
+
+set.seed(123)
+
+dataset <- read.csv('train.csv')
+indexes = createDataPartition(dataset$y, p = .85, list = F)
+
+train = dataset[indexes,]
+test = dataset[-indexes,]
+
+xtrain = as.matrix(train[,-17])
+ytrain = as.matrix(train[,17])
+xtest = as.matrix(test[,-17])
+ytest = as.matrix(test[, 17])
+
+dim(xtrain)
+
+dim(ytrain)
+
+xtrain = array(xtrain, dim = c(nrow(xtrain), 16, 1))
+xtest = array(xtest, dim = c(nrow(xtest), 16, 1))
+
+dim(xtrain)
+dim(xtest)
+
+in_dim = c(dim(xtrain)[2:3])
+print(in_dim)
+
+model = keras_model_sequential() %>%
+  layer_conv_1d(filters = 64, kernel_size = 2,
+                input_shape = in_dim, activation = "relu") %>%
+  layer_flatten() %>%
+  layer_dense(units = 32, activation = "relu") %>%
+  layer_dense(units = 1, activation = "linear")
+
+model %>% compile(
+  loss = "mse",
+  optimizer = "adam")
+
+model %>% summary()
+
+model %>% fit(xtrain, ytrain, epochs = 100, batch_size=16, verbose = 0)
+scores = model %>% evaluate(xtrain, ytrain, verbose = 0)
+print(scores)
+
+ypred = model %>% predict(xtest)
+
+cat("RMSE:", RMSE(ytest, ypred))
+
+x_axes = seq(1:length(ypred))
+plot(x_axes, ytest, ylim = c(min(ypred), max(ytest)),
+     col = "burlywood", type = "l", lwd = 2, ylab = "medv")
+lines(x_axes, ypred, col = "red", type = "l", lwd = 2)
+legend("topleft", legend = c("y-test", "y-pred"),
+       col = c("burlywood", "red"), lty=1, cex=0.7, lwd=2, bty='n')
